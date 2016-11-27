61bc624e788e64eaba13478d1d187d21837cfab4
nichjang <nich.jang@gmail.com>
2016-11-26 22:02:30 -0600

    Added another function

--- normalize_csv.py
+++ normalize_csv.py
@@ -1,17 +1,35 @@
 import csv
 import clean_string
 
-list = []
-filename = 'salaries11.19.csv'
+filename1 = 'salaries11.19.csv'
+filename2 = 'results11.19.csv'
 
-with open(filename, 'r') as csvf:
-	csvreader = csv.reader(csvf, delimiter=',', lineterminator='\n')
-	for row in csvreader:
-		cleaned_string = clean_string.cleanstr(str(row[1]))
-		row[1] = cleaned_string
-		list.append(row)
+def clean_salaries():
+	list = []
+	with open(filename1, 'r') as csvf:
+		csvreader = csv.reader(csvf, delimiter=',', lineterminator='\n')
+		for row in csvreader:
+			cleaned_string = clean_string.cleanstr(str(row[1]))
+			row[1] = cleaned_string
+			list.append(row)
 
-with open(filename, 'w') as csvf:
-	csvwriter = csv.writer(csvf, delimiter=',', lineterminator='\n')
-	for row in list:
-		csvwriter.writerow(row)
+	with open(filename1, 'w') as csvf:
+		csvwriter = csv.writer(csvf, delimiter=',', lineterminator='\n')
+		for row in list:
+			csvwriter.writerow(row)
+
+def clean_results():
+	list = []
+	with open(filename2, 'r') as csvf:
+		csvreader = csv.reader(csvf, delimiter=',', lineterminator='\n')
+		for row in csvreader:
+			cleaned_string = clean_string.cleanstr(str(row[0]))
+			row[0] = cleaned_string
+			list.append(row)
+
+	with open(filename2, 'w') as csvf:
+		csvwriter = csv.writer(csvf, delimiter=',', lineterminator='\n')
+		for row in list:
+			csvwriter.writerow(row)
+
+clean_results()
