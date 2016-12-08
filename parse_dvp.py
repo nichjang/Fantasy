922d853095eb75052f23ac68ebcfddb6c2b42d78
nichjang <nich.jang@gmail.com>
2016-12-08 13:54:14 -0600

    Bug fixes and function improvement

--- parse_dvp.py
+++ parse_dvp.py
@@ -11,11 +11,6 @@ from bs4 import BeautifulSoup
 		f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
 		print(*map(f, objects), sep=sep, end=end, file=file)"""
 
-baseurl = 'http://www.rotowire.com/daily/nba/defense-vspos.htm?site=DraftKings&statview=last5'
-endurl = ['&pos=SG','&pos=G','&pos=SF','&pos=PF','&pos=F','&pos=C','na']
-next_page = str(baseurl)
-list = []
-
 #dictionary mapping: key = team, value = team abbrev
 def abbrev(name):
 	return {
@@ -51,36 +46,43 @@ def abbrev(name):
 		'Portland Trail Blazers': 'Por'
 	}[name]
 
-for i in range(7):
-	while True:
-		r = requests.get(next_page)
-		soup = BeautifulSoup(r.content,'html.parser')
-		table = soup.find('tbody')
+def parse():
+	baseurl = 'http://www.rotowire.com/daily/nba/defense-vspos.htm?site=DraftKings&statview=last5'
+	endurl = ['&pos=SG','&pos=G','&pos=SF','&pos=PF','&pos=F','&pos=C','na']
+	next_page = str(baseurl)
+	list = []
 
-		for row in table.findAll('tr'):
-			cells = [c.get_text() for c in row.findAll('td')]
-			stringcells = str(cells[0])
-			cells[0] = abbrev(stringcells)
-			del cells[4:]
-			del cells[2]
-			cells.append(opponent(stringcells))
-			list.append(cells)
-		break;
-	next_page = str(baseurl) + str(endurl[i])
+	for i in range(7):
+		while True:
+			r = requests.get(next_page)
+			soup = BeautifulSoup(r.content,'html.parser')
+			table = soup.find('tbody')
 
-for i in range(7):
-	dvp_percents = []
-	sum = 0.0
-	for j in range(1,31):
-		dvp_percents.append(list[(i*30)+j-1][2])
-		sum += float(list[(i*30)+j-1][2])
-	sum /= 30
-	print(sum)
-	for j in range(1,31):
-		list[(i*30)+j-1][2] = (float(dvp_percents[j-1])-sum)/sum
+			for row in table.findAll('tr'):
+				cells = [c.get_text() for c in row.findAll('td')]
+				stringcells = str(cells[0])
+				cells[0] = abbrev(stringcells)
+				del cells[4:]
+				del cells[2]
+				list.append(cells)
+			break;
+		next_page = str(baseurl) + str(endurl[i])
+
+	for i in range(7):
+		dvp_percents = []
+		sum = 0.0
+		for j in range(1,31):
+			dvp_percents.append(list[(i*30)+j-1][2])
+			sum += float(list[(i*30)+j-1][2])
+		sum /= 30
+		print(sum)
+		for j in range(1,31):
+			list[(i*30)+j-1][2] = (float(dvp_percents[j-1])-sum)/sum
 
 
-with open('dvp.csv', 'w') as csvf:
-	csvwriter = csv.writer(csvf, delimiter=',', lineterminator='\n')
-	for i in range(len(list)):
-		csvwriter.writerow(list[i])
+	with open('dvp.csv', 'w') as csvf:
+		csvwriter = csv.writer(csvf, delimiter=',', lineterminator='\n')
+		for i in range(len(list)):
+			list[i][1] = list[i][1].replace(' ','')
+			csvwriter.writerow(list[i])
+#parse()
