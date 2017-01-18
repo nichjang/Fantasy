bfc893c22c543af883e04726d364bb7870ede26e
Nicholas Jang <nich.jang@gmail.com>
2017-01-18 16:55:43 -0600

    Update espn_to_db.py

--- espn_to_db.py
+++ espn_to_db.py
@@ -55,41 +55,28 @@ def execute_range():
 			cur.execute("COPY (SELECT distinct(player),threepm,reb,ast,stl,block,pts,actualpoints,teampoints,teamagainstpoints,dvp FROM espnlastfive WHERE salary > 2999) TO \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\data_predict%s.csv' With CSV DELIMITER ',';" % str(cur_date))
 	conn.close()
 
-#These functions only need to be executed once
-"""
-parse_espn.parse_single(espn_date) #only if necessary
-parse_dvp.parse()
-parse_game.parse(game_date)
-format_csv.format_sal(game_date_start,game_date_end)
-format_csv.format_results(game_date_start,game_date_end)
-"""
 def all_dates():
 	truncate_everything()
 	import_dates_all(38)
 	conn.commit()
-	#cur.execute("COPY (SELECT distinct(player),dkp,teampoints,teamagainstpoints,dvp FROM espnlastfive WHERE salary > 2999) TO \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\data_predict.csv' With CSV DELIMITER ',';")
 	conn.close()
 
 def execute_once():
 	cur_date = 35
 
 	truncate_everything()
-	#parse_game.parse(cur_date)
 	parse_game.parse('06')
 	import_dates(cur_date+7)
 	last_five()
 	cur.execute("COPY gameinfo(team,teampoints,teamagainst) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\gameinfo.csv\' DELIMITER ',' CSV")
-	#cur.execute("COPY currentdk(position,name,salary,gameinfo,avgpoints,teamabbrev) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\salaries11.%sformat.csv\' DELIMITER ',' CSV" % str(cur_date))
 	cur.execute("COPY currentdk(position,name,salary,gameinfo,avgpoints,teamabbrev) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\salaries\\salaries12.6.format.csv\' DELIMITER ',' CSV")
 	cur.execute("COPY dvp(team,position,dvp_percent) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\dvp.csv\' DELIMITER ',' CSV")
-	#cur.execute("COPY results(player,actualpoints) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\results11.%sformat.csv\' DELIMITER ',' CSV" % str(cur_date))
 	cur.execute("COPY results(player,actualpoints) FROM \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\results\\results12.6.csv\' DELIMITER ',' CSV")
 	cur.execute("SELECT update_salary()")
 	cur.execute("SELECT update_results()")
 	cur.execute("SELECT update_game()")
 	cur.execute("SELECT stddev()")
 	conn.commit()
-	#cur.execute("COPY (SELECT distinct(player),threepm,reb,ast,stl,block,pts,teampoints,teamagainstpoints,dvp FROM espnlastfive WHERE salary > 2999) TO \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\data_predict.csv' With CSV DELIMITER ',';")
 	cur.execute("COPY (SELECT player,dkp,salary,position,gp,actualpoints,stddev FROM espnlastfive WHERE salary>2999) TO \'C:\\Users\\Nicholas\\Documents\\GitHub\\Fantasy\\xy.csv' With CSV DELIMITER ',';")
 	conn.close()
 
